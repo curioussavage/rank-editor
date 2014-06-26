e12a10abb75fedeb2bbec61cf06be0b5e2a7a6ea
Adam <adambwinn@gmail.com>
2014-06-26 00:11:06 -0600

    add jsonfetch file

--- app/scripts/services/jsonfetch.js
+++ app/scripts/services/jsonfetch.js
@@ -4,19 +4,22 @@ angular.module('rankEditorApp')
   .service('Json', function Json($q,$timeout, $http) {
 
         return {
-            fetch: function(file) {   // had callback as a parameter
+            // fetch: function(file) {   // had callback as a parameter
 
-                var deferred = $q.defer();
+            //     var deferred = $q.defer();
 
-                $timeout(function() {
-                    $http.get(file).success(function(data) {  // was 'convertcsv.json' manually entered
-                        deferred.resolve(data);
+            //     $timeout(function() {
+            //         $http.get(file).success(function(data) {  // was 'convertcsv.json' manually entered
+            //             deferred.resolve(data);
 
-                    });
-                },30);
+            //         });
+            //     },30);
 
-                return deferred.promise;
-            },
+            //     return deferred.promise;
+            // },
+
+
+
 
         };
 
